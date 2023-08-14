b3302685ad3199bba5067b15f4ef00d4b9c7adab
Vadim <fw.mbagg@gmail.com>
2023-08-14 19:03:44 +0300

    "Stop gninnipS My sdroW!" update

--- src/main/java/codewars/CodeWarsTasks.java
+++ src/main/java/codewars/CodeWarsTasks.java
@@ -8,6 +8,13 @@ public class CodeWarsTasks {
         System.out.println(spinWords("Привет всем"));
     }
 
+    /*
+    Task: "Stop gninnipS My sdroW!"
+    Write a function that takes in a string of one or more words,
+    and returns the same string, but with all five or more letter words reversed.
+    Strings passed in will consist of only letters and spaces.
+    Spaces will be included only when more than one word is present.
+    */
     public static String spinWords(String sentence) {
         return Arrays.stream(sentence.trim().split(" "))
                 .map(x -> {
